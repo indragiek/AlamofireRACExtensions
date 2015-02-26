ed300096657eb29e53bd6b0a9bfe1ed28cdad853
Indragie Karunaratne <i@indragie.com>
2015-02-25 18:51:12 -0700

    Initial Commit

---
+++ AlamofireRACExtensions/AlamofireRACExtensions.h
@@ -0,0 +1,19 @@
+//
+//  AlamofireRACExtensions.h
+//  AlamofireRACExtensions
+//
+//  Created by Indragie on 2/25/15.
+//  Copyright (c) 2015 Indragie Karunaratne. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AlamofireRACExtensions.
+FOUNDATION_EXPORT double AlamofireRACExtensionsVersionNumber;
+
+//! Project version string for AlamofireRACExtensions.
+FOUNDATION_EXPORT const unsigned char AlamofireRACExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlamofireRACExtensions/PublicHeader.h>
+
+
